af57b4b0dd1bd7955eb568489482c7253e4c391b
ZeNoonan <darragh.noonan@gmail.com>
2021-12-04 09:52:54 +0000

    update

--- premier_league.py
+++ premier_league.py
@@ -88,8 +88,8 @@ with st.beta_expander('Mins'):
     
     st.write( player_selected_detail_by_week.sort_values(by=['year','week'],ascending=[False,False]) )
 
-    week_mins = 13
-    current_week=14
+    week_mins = 14
+    current_week=15
     df_1= data_2022 [ (data_2022['week']==week_mins) ].sort_values(by='Price',ascending=False)
     df_1=df_1.loc[:,['full_name','week','year','Price','4_games_rolling_mins','team']]
     df_1['week']=week_mins+1
