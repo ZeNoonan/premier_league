48f8cfadfc6e998d19c8e0a8ec82e19ec01c0aeb
ZeNoonan <darragh.noonan@gmail.com>
2021-12-31 13:22:58 +0000

    update

--- fpl_picker.py
+++ fpl_picker.py
@@ -17,22 +17,42 @@ with st.expander('Data Prep'):
     file_location_2022='C:/Users/Darragh/Documents/Python/premier_league/raw_data_2022.csv'
     file_location_2021='C:/Users/Darragh/Documents/Python/premier_league/raw_data_2021.csv'
     file_location_2020='C:/Users/Darragh/Documents/Python/premier_league/raw_data_2020.csv'
+    
 
     url2022 = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2021-22/players_raw.csv'
     url2021 = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2020-21/players_raw.csv'
     url2020 = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2019-20/players_raw.csv'
 
-    col_selection_url=['id','element_type','first_name','second_name','team']
-    @st.cache
+    col_selection_url=['id','element_type','first_name','second_name']
+    col_selection_url_2020=['id','element_type','first_name','second_name','team']
+    # st.write('url master data 2020', pd.read_csv(url2020).head())
+
+    @st.cache(suppress_st_warning=True)
     def read_data(x,col_selection):
         return pd.read_csv(x,usecols=col_selection)
 
+    @st.cache(suppress_st_warning=True)
+    def data_2020_team_names(file):
+        file['team'] = file['team'].map({1: 'Arsenal', 2: 'Aston_Villa', 3:'Bournemouth', 4:'Brighton',5:'Burnley',6:'Chelsea',7:'Crystal_Palace',
+        8:'Everton',9:'Leicester',10:'Liverpool',11:'Man_City',12:'Man_Utd',13:'Newcastle',14:'Norwich',15:'Sheffield_Utd',16:'Southampton',17:'Spurs',
+        18:'Watford',19:'West_Ham',20:'Wolves'})
+        return file
+
     col_selection_week=['team','bps','bonus','player_id','ict_index','minutes','opponent_team','selected','total_points','transfers_in','transfers_out',
+    'value','week','year']
+    col_selection_week_2020=['bps','bonus','player_id','ict_index','minutes','opponent_team','selected','total_points','transfers_in','transfers_out',
     'value','week','year']
     url_csv_2022=read_data(url2022,col_selection_url)
     url_csv_2021=read_data(url2021,col_selection_url)
+    url_csv_2020=read_data(url2020,col_selection_url_2020).copy()
+    url_csv_2020=data_2020_team_names(url_csv_2020)
+    # url_csv_2020=data_2020_team_names((read_data(url2020,col_selection_url_2020).copy()))
+    # st.write('master data 2020',url_csv_2020.head())
     df_week_data_raw_2022 = read_data(file_location_2022,col_selection_week)
     df_week_data_raw_2021 = read_data(file_location_2021,col_selection_week)
+    # st.write('2020 raw data weekly',pd.read_csv(file_location_2020).head())
+    df_week_data_raw_2020 = read_data(file_location_2020,col_selection_week_2020)
+    # st.write(df_week_data_raw_2022.head())
 
     @st.cache
     def prep_base_data(url_csv, pick):
@@ -43,20 +63,21 @@ with st.expander('Data Prep'):
         return pd.merge(url_csv,pick_data, on='player_id',how ='outer')
 
     @st.cache(suppress_st_warning=True)
-    def data_2022_team_names(file):
-        file['team'] = file['team'].map({1: 'Arsenal', 2: 'Aston_Villa', 3: 'Brentford', 4:'Brighton', 5:'Burnley',6:'Chelsea',7:'Crystal_Palace',8:'Everton',
-        9:'Leicester',10:'Leeds_Utd',11:'Liverpool',12:'Man_City',13:'Man_Utd',14:'Newcastle',15:'Norwich',16:'Southampton',17:'Spurs',
-        18:'Watford',19:'West_Ham',20:'Wolves'})
-        return file
+    def data_2020_clean_double_gw(url_pick2020):
+        url_pick2020=url_pick2020[ ~(url_pick2020['round']==29) | ~(url_pick2020['fixture']==275)]
+        url_pick2020.loc[:,'week']=url_pick2020['week'].replace({39:30,40:31,41:32,42:33,43:34,44:35,45:36,46:37,47:38})
+        url_pick2020.loc[:,'round']=url_pick2020['round'].replace({39:30,40:31,41:32,42:33,43:34,44:35,45:36,46:37,47:38})
+        gw_18_blank = clean_blank_gw(url_pick2020,10,19,17)
+        gw_28_blank = clean_blank_gw(url_pick2020,11,3,27)
+        gw_28_blank_1 = clean_blank_gw(url_pick2020,2,15,27)
+        return pd.concat([url_pick2020,gw_18_blank,gw_28_blank,gw_28_blank_1])    
 
-    @st.cache(suppress_st_warning=True)
-    def data_2021_team_names(file):
-        file['team'] = file['team'].map({1: 'Arsenal', 2: 'Aston_Villa', 3:'Brighton', 4:'Burnley',5:'Chelsea',6:'Crystal_Palace',7:'Everton',
-        8:'Fulham',9:'Leicester',10:'Leeds_Utd',11:'Liverpool',12:'Man_City',13:'Man_Utd',14:'Newcastle',15:'Sheffield_Utd',16:'Southampton',17:'Spurs',
-        18:'West_Brow',19:'West_Ham',20:'Wolves'})
-        return file
+    data_2022 = (( (prep_base_data(url_csv_2022, df_week_data_raw_2022))))
+    data_2021 = (( (prep_base_data(url_csv_2021, df_week_data_raw_2021))))
+    data_2020 = (( (prep_base_data(url_csv_2020, df_week_data_raw_2020)))).copy()
+    # data_2020 = data_2020_clean_double_gw(data_2020)
 
-    data_2022 = (data_2022_team_names( (prep_base_data(url_csv_2022, df_week_data_raw_2022)).rename(columns = {'team_x':'team'})))
-    data_2021 = (data_2021_team_names( (prep_base_data(url_csv_2021, df_week_data_raw_2021)).rename(columns = {'team_x':'team'})))
     st.write(data_2022.head())
+    st.write(data_2021.head())
+    st.write(data_2020[data_2020['full_name'].str.contains('salah')])
 
