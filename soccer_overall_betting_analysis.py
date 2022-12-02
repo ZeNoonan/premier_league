acc5609d24b669123fab432bb42988912c97c34b
ZeNoonan <darragh.noonan@gmail.com>
2022-12-02 11:39:20 +0000

    update

--- soccer_overall_betting_analysis.py
+++ soccer_overall_betting_analysis.py
@@ -8,6 +8,7 @@ import seaborn as sns
 
 st.set_page_config(layout="wide")
 
+@st.cache
 def read_file(file,league='la_liga',season=2023):
     df=pd.read_csv(file)
     df['league']=league
@@ -23,6 +24,17 @@ factor_serie_a_2021_2022=read_file('C:/Users/Darragh/Documents/Python/premier_le
 factor_bundesliga_2022_2023=read_file('C:/Users/Darragh/Documents/Python/premier_league/bundesliga_factor_result_2022_2023.csv','bundesliga',2023)
 factor_bundesliga_2021_2022=read_file('C:/Users/Darragh/Documents/Python/premier_league/bundesliga_factor_result_2021_2022.csv','bundesliga',2022)
 
-st.write(factor_bundesliga_2021_2022)
 # combine file above into one dataframe
-combined_df=pd.concat([factor_la_liga_2022_2023,factor_la_liga_2021_2022,factor_premier_league_2021_2022])
+combined_df=pd.concat([factor_la_liga_2022_2023,factor_la_liga_2021_2022,factor_premier_league_2021_2022,factor_premier_league_2022_2023,
+factor_serie_a_2022_2023,factor_serie_a_2021_2022,factor_bundesliga_2022_2023,factor_bundesliga_2021_2022])
+
+# st.write(combined_df)
+# pivot table on dataframe
+df_pivot=pd.pivot_table(combined_df,values=['total_turnover','total_season_cover','power_ranking_success?'],index=['index','season'],aggfunc=np.sum)
+df_pivot_1=pd.pivot_table(combined_df,values=['total_turnover','total_season_cover','power_ranking_success?'],index=['index'],aggfunc=np.sum)
+
+df_pivot.loc[('% Winning',2022)] = (df_pivot.loc[('Winning_Bets',2022)] / (df_pivot.loc[('Winning_Bets',2022)]+df_pivot.loc[('Losing_Bets',2022)])  )
+df_pivot.loc[('% Winning',2023)] = (df_pivot.loc[('Winning_Bets',2023)] / (df_pivot.loc[('Winning_Bets',2023)]+df_pivot.loc[('Losing_Bets',2023)])  )
+df_pivot_1.loc['% Winning'] = (df_pivot_1.loc['Winning_Bets'] / (df_pivot_1.loc['Winning_Bets']+df_pivot_1.loc['Losing_Bets'])  )
+st.write(df_pivot)
+st.write(df_pivot_1)
